b78cdb58e97866cfd7cd2bb18a52643c1a31d3f7
rishy-2021 <riteshpuwar2001@gmail.com>
2025-01-23 00:41:49 +0530

    update error handler

--- utils/responseHandler.js
+++ utils/responseHandler.js
@@ -1,8 +1,16 @@
-export const createResponse = ({ status = "200", result = null, message = "", error = "" }) => {
-    return {
-      status,
-      result,
-      message,
-      error,
-    };
-  };
+export const createResponse = ({
+  status = "200",
+  result = null,
+  message = "",
+  error = "",
+}) => {
+  const response = { status, message };
+
+  if (error) {
+    response.error = error;
+  } else if (result) {
+    response.result = result;
+  }
+
+  return response;
+};
